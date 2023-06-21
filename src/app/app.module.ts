c2bb8603ed0b8f34eb75d7de4c6d72dc65a17330
Christopher Q <fufales@gmail.com>
2023-06-21 13:33:19 -0600

    add mobile and desktop templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestingLpComponent } from './testing-lp/testing-lp.component';
+import { TestingLpMobileComponent } from './testing-lp-mobile/testing-lp-mobile.component';
+import { TestingLpDesktopComponent } from './testing-lp-desktop/testing-lp-desktop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestingLpComponent
+    TestingLpComponent,
+    TestingLpMobileComponent,
+    TestingLpDesktopComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/testing-lp-mobile/testing-lp-mobile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-lp-mobile',
+  templateUrl: './testing-lp-mobile.component.html',
+  styleUrls: ['./testing-lp-mobile.component.css']
+})
+export class TestingLpMobileComponent {
+
+}
